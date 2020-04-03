3b4d57eb19c98379acfde091482a50cfb23aad79
oumi040720 <nguyenhoang04072000@gmail.com>
2020-04-03 09:49:46 +0700

    load quiz and paging it

--- ASM/js/QuizController.js
+++ ASM/js/QuizController.js
@@ -3,7 +3,25 @@ app.controller("quizCtrl", function ($scope, $http, $routeParams) {
     var url = "db/Quizs/" + id + ".js";
     $http.get(url).then(function (response) {
         $scope.questions = response.data;
-        console.log($scope.questions);
 
+        $scope.begin = 0;
+        $scope.page = $scope.questions.length;
+
+        $scope.first = function () {
+            $scope.begin = 0;
+        }
+        $scope.prev = function () {
+            if ($scope.begin > 0) {
+                $scope.begin -= 1;
+            }
+        }
+        $scope.next = function () {
+            if ($scope.begin < ($scope.page - 1)) {
+                $scope.begin += 1;
+            }
+        }
+        $scope.last = function () {
+            $scope.begin = ($scope.page - 1);
+        }
     })
 })
