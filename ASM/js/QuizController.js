3d5c4e5f52892b972b1617028b9f69fbaa77b89e
oumi040720 <nguyenhoang04072000@gmail.com>
2020-04-04 09:32:53 +0700

    clone quiz app example and load quiz limit 10 question but not test unique

--- ASM/js/QuizController.js
+++ ASM/js/QuizController.js
@@ -5,7 +5,35 @@ app.controller("quizCtrl", function ($scope, $http, $routeParams) {
         $scope.questions = response.data;
 
         $scope.begin = 0;
-        $scope.page = $scope.questions.length;
+        $scope.page = 10;
+
+        var arrNo = [];
+        for (let i = 0; i < 10; i++) {
+            arrNo.push(parseInt(Math.floor(Math.random() * $scope.questions.length) + 1));
+        }
+
+        $scope.quizs = [];
+
+        for (let i = 0; i < $scope.questions.length; i++) {
+            for (let j = 0; j < arrNo.length; j++) {
+                if (i == arrNo[j]) {
+                    var quiz = {
+                        "No": $scope.quizs.length + 1,
+                        "Id": $scope.questions[i].Id,
+                        "Text": $scope.questions[i].Text,
+                        "Answers": $scope.questions[i].Answers,
+                        "AnswerID": $scope.questions[i].AnswerId,
+                        "yourAnswer": "",
+                    };
+                    $scope.quizs.push(quiz);
+                }
+            }
+        }
+        console.log($scope.quizs);
+
+        $scope.clickAnswer = function () {
+
+        }
 
         $scope.first = function () {
             $scope.begin = 0;
